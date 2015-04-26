7301e989fa6f9e8b3652c5e0a94a54ac5baa61bf
minghuiyu <minghuiyu@gmail.com>
2015-04-26 13:29:53 -0700

    Completed version
    compare new matrix assigned via set(). If new matrix, inversion is reset
    to NULL.

--- cachematrix.R
+++ cachematrix.R
@@ -5,30 +5,35 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  original <- NULL
-  if (is.null(original)) original<<-TRUE
-  isOriginal <- function() original
-  
-  isOriginal <- NULL
   inversion <- NULL
-  identical <- TRUE
+  originalMatrix <- x ## used to determine if matrix has been changed
+  
+  ## return matrix
   get <- function() x
   
+  ## set matrix
   set <- function(y) {
-    ##if set is called, test if new assigned object is identical to the original object
-    identical <<- identical(y,original)
     x <<- y
+    if (!identical(y,originalMatrix)) {
+      message("new matrix assined. Inversion is reset to NULL")
+      inversion <<- NULL
+    } else {
+      message("new matrix is identical to *original* (may or may not be the previous) matrix")
+      message("Inversion matrix is NOT reset to null")
+    }
   }
   
-  getInversion <- function() inversion<<-solve(x)
-  ## return TRUE if newly assigned object is identical to the previous one.Default is TRUE
-  ifIdentical <- function() identical
+  ## set matrix's inversion
+  setInversion <- function(matrix) inversion <<- matrix
+  
+  ## return matrix's inversion
+  getInversion <- function() inversion
+  
   list(set = set, 
        get = get, 
        getInversion = getInversion, 
-       ifIdentical = ifIdentical
-       )
-  
+       setInversion = setInversion
+       )  
 }
 
 
@@ -36,12 +41,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  ##if identical=TRUE AND inversion is NOT NULL, return cached inversion
-  if (x$ifIdentical()==TRUE) {
-    return(TRUE)
-  }else{
-    return(FALSE)
+  
+  ##if inversion is NOT NULL, return cached inversion
+  if (!is.null(x$getInversion())) {
+    message("returning cached inversion")
+    return(x$getInversion())
   }
   ##otherwise compute inversion and return it
-  
+  matrix <- x$get()
+  inv <- solve(matrix)
+  x$setInversion(inv)
+  inv 
 }
