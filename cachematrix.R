3df2f212f0126e6e8651d87059fa42dfe6870f45
minghuiyu <minghuiyu@gmail.com>
2015-04-26 11:13:13 -0700

    Initial commit
    Have not completed yet

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  original <- NULL
+  if (is.null(original)) original<<-TRUE
+  isOriginal <- function() original
+  
+  isOriginal <- NULL
+  inversion <- NULL
+  identical <- TRUE
+  get <- function() x
+  
+  set <- function(y) {
+    ##if set is called, test if new assigned object is identical to the original object
+    identical <<- identical(y,original)
+    x <<- y
+  }
+  
+  getInversion <- function() inversion<<-solve(x)
+  ## return TRUE if newly assigned object is identical to the previous one.Default is TRUE
+  ifIdentical <- function() identical
+  list(set = set, 
+       get = get, 
+       getInversion = getInversion, 
+       ifIdentical = ifIdentical
+       )
+  
 }
 
 
@@ -12,4 +36,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ##if identical=TRUE AND inversion is NOT NULL, return cached inversion
+  if (x$ifIdentical()==TRUE) {
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+  ##otherwise compute inversion and return it
+  
 }
